55f693c8bf84dc86e9a3565af4ff3a99e55165f8
Stephy Cat <stephycat@link.cuhk.edu.hk>
2019-08-28 17:58:35 +0800

    ALT: class `sort_link` renamed to `sort-link`
    to aligning with CSS naming conventions

--- lib/ransack/helpers/form_helper.rb
+++ lib/ransack/helpers/form_helper.rb
@@ -138,7 +138,7 @@ module Ransack
         def html_options(args)
           html_options = extract_options_and_mutate_args!(args)
           html_options.merge(
-            class: [['sort_link'.freeze, @current_dir], html_options[:class]]
+            class: [['sort-link'.freeze, @current_dir], html_options[:class]]
                    .compact.join(' '.freeze)
           )
         end
